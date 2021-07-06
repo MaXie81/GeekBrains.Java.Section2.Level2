3850f972cbc3168725f859d139ba0e5921df1003
KrakenUser <KrakenUser@yandex.ru>
2021-07-06 07:52:57 +0300

    Добавлен Модуль "Сообщения"(Message)

---
+++ Message/src/main/java/message/Mess.java
@@ -0,0 +1,45 @@
+package message;
+
+import com.google.gson.Gson;
+import dictionary.ResultCodes;
+import dictionary.CommandCodes;
+import dictionary.SelectTypes;
+import dictionary.MessageTypes;
+import lombok.Data;
+
+import java.util.LinkedList;
+@Data
+public class Mess {
+    private MessageTypes type;
+    private CommandCodes command = CommandCodes.NOT_DEFINED;
+    private boolean flgServer = false;
+
+    private String dirName;
+    private String dirPath;
+    private boolean flgDirRoot;
+    private String selectName;
+    private SelectTypes selectType;
+    private LinkedList<String> listFile;
+
+    private String login;
+    private String password;
+
+    private int cntClick;
+//    private int errCode = 0;
+    private ResultCodes code = ResultCodes.OK;
+
+    private String valSting = "";
+    private int valInt;
+    private long valLong;
+    private boolean flg;
+
+    public Mess(MessageTypes type) {
+        this.type = type;
+    }
+    public static Mess fromJson(String json) {
+        return new Gson().fromJson(json, Mess.class);
+    }
+    public String toJson() {
+        return new Gson().toJson(this);
+    }
+}
---
+++ Message/src/main/java/message/MessUtil.java
@@ -0,0 +1,47 @@
+package message;
+
+import dictionary.MessageTypes;
+import dictionary.ResultCodes;
+
+import java.util.HashMap;
+
+public class MessUtil {
+    private static HashMap<MessageTypes, MessageTypes> mapMessResp = new HashMap<>();
+    static {
+        mapMessResp.put(MessageTypes.AUTH_ON, MessageTypes.AUTH_ON_RESP);
+        mapMessResp.put(MessageTypes.AUTH_OFF, MessageTypes.AUTH_OFF_RESP);
+        mapMessResp.put(MessageTypes.CONN_CLOSE, MessageTypes.CONN_CLOSE_RESP);
+        mapMessResp.put(MessageTypes.DIR_SET, MessageTypes.DIR_SET_RESP);
+        mapMessResp.put(MessageTypes.DIR_INFO, MessageTypes.DIR_INFO_RESP);
+        mapMessResp.put(MessageTypes.DIR_COPY, MessageTypes.DIR_COPY_RESP);
+        mapMessResp.put(MessageTypes.DIR_ADD, MessageTypes.DIR_ADD_RESP);
+        mapMessResp.put(MessageTypes.DIR_DEL, MessageTypes.DIR_DEL_RESP);
+
+    }
+    public static boolean isRespOK(Mess mess, Mess messResp) {
+        if (messResp.getType() != mapMessResp.get(mess.getType())) return false;
+        if (messResp.getCommand() != mess.getCommand()) return false;
+        if (messResp.getCode() != ResultCodes.OK) return false;
+        return true;
+    }
+    public static Mess getErr(ResultCodes code) {
+        Mess mess = new Mess(MessageTypes.ERR);
+        mess.setCode(code);
+        return mess;
+    }
+    public static Mess getRespErr(ResultCodes code) {
+        Mess messResp = new Mess(MessageTypes.ERR_RESP);
+        messResp.setCode(code);
+        return messResp;
+    }
+    public static Mess getResp(Mess mess, ResultCodes code) {
+        Mess messResp = Mess.fromJson(mess.toJson());
+        messResp.setType(mapMessResp.get(mess.getType()));
+        messResp.setCode(code);
+
+        return messResp;
+    }
+    public static Mess getRespOk(Mess mess) {
+        return getResp(mess, ResultCodes.OK);
+    }
+}
